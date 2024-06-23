695c40f27bfa51e2935d00b144a4537e944ba568
Devlepa <juanestebanleonparra@outlook.es>
2024-06-23 14:59:16 -0500

    Upload Blog Post Github v2

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = "djangoproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 DATABASES = {
     "default": {
